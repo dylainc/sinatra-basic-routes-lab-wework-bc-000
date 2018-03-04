9426359e8c5c762c17571f416e795c8959d19b3d
Dylain Cohen <dylain.cohen@wework.com>
2018-03-04 23:47:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,17 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Dylain"
+  end
+
+  get '/hometown' do
+    "My hometown is NYC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Zambuk"
+  end  
 end
 
 
-get '/name' do
-  "My name is Dylain"
-end
 
-get '/hometown' do
-  "My hometown is NYC"
-end
-
-get '/favorite-song' do
-  "My favorite song is Zambuk"
-end
